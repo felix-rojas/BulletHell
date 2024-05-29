be156c1cd826b1518831eeb1685d11cfcac0dd24
felix-rojas <a01201946@tec.mx>
2024-05-29 11:17:47 -0600

    add HPDisplay class

---
+++ Assets/Scripts/HPDisplay.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HPDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI hpTextDisplay;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}

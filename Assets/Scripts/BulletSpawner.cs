816899e03dff5a68bc57770505b9546f61da3784
felix-rojas <a01201946@tec.mx>
2024-05-29 09:34:28 -0600

    Add BulletSpawner

--- Assets/Scripts/BulletSpawner.cs
+++ Assets/Scripts/BulletSpawner.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public float minRotation;
+    public float maxRotation;
+    public int numberOfBullets;
+
+    public float spawnCooldown;
+    float timer;
+    public float bulletSpeed;
+    public Vector3 bulletVelocity;
+
+    float[] rotations;
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = spawnCooldown;
+        rotations = new float[numberOfBullets];
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timer <= 0)
+        {
+            spawnBullets();
+            timer = spawnCooldown;
+        }
+        timer -= Time.deltaTime;
+    }
+
+    public float[] BulletDistributions()
+    {
+        for (int i = 0; i < numberOfBullets; i++)
+        {
+            var fraction = (float)i / (float)numberOfBullets;
+            var diff = maxRotation - minRotation;
+            var fractionDiff = fraction * diff;
+            rotations[i] = fractionDiff * minRotation;
+        }
         
+        return rotations;
+    }
+
+    public GameObject[] spawnBullets()
+    {
+        GameObject[] spawnedBullets = new GameObject[numberOfBullets];
+        for (int i = 0; i < numberOfBullets; i++)
+        {
+            spawnedBullets[i] = Instantiate(bulletPrefab, transform);
+
+            var b = spawnedBullets[i].GetComponent<Bullet>;
+            b.rotation = rotations[i];
+            b.speed = bulletSpeed;
+            b.velocity = bulletVelocity;
+        }
+        return spawnedBullets;
     }
 }

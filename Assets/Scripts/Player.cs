60e2342c09945e2aca74e12a4cc4dd2769c185ae
felix-rojas <a01201946@tec.mx>
2024-05-29 08:39:16 -0600

    Add project files.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int startHP;
+    int HP;
+    // hit invuln
+    public float bulletCooldown;
+    float bulletTimer;
+    void Start()
+    {
+        HP = startHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletTimer -= Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Bullet" && bulletTimer <= 0)
+        {
+            HP -= 1;
+            bulletTimer = bulletCooldown;
+        }
+    }
+}
